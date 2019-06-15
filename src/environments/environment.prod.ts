603ff16b0b97d44b6ca36ecd1673e480d04111d1
Rajwant Prajapati <rajwantprajapati11@gmail.com>
2019-06-15 15:59:40 +0530

    initial set up and bootstrap set up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAiGA32szQeTJDCaNvjTc10-z7_Hh_cfC8",
+    authDomain: "oshop-3d0b6.firebaseapp.com",
+    databaseURL: "https://oshop-3d0b6.firebaseio.com",
+    projectId: "oshop-3d0b6",
+    storageBucket: "oshop-3d0b6.appspot.com",
+    messagingSenderId: "816323179379",
+    appId: "1:816323179379:web:f78cc6a739f6c713"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAiGA32szQeTJDCaNvjTc10-z7_Hh_cfC8",
+    authDomain: "oshop-3d0b6.firebaseapp.com",
+    databaseURL: "https://oshop-3d0b6.firebaseio.com",
+    projectId: "oshop-3d0b6",
+    storageBucket: "oshop-3d0b6.appspot.com",
+    messagingSenderId: "816323179379",
+    appId: "1:816323179379:web:f78cc6a739f6c713"
+  }
 };
 
 /*
