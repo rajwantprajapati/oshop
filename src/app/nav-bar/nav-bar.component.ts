2614ddf0c466101213d7964e554fe1f41bab9a26
Rajwant Prajapati <rajwantprajapati11@gmail.com>
2019-06-20 23:37:13 +0530

    adds auth gaurd and authentication

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AppUser } from './../models/app-user';
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
+  appUser: AppUser;
+  constructor(private auth: AuthService) {
+    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  logout() {
+    this.auth.logout();
   }
 
 }
